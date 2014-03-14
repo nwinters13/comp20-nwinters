5fee59aaab040d83c9cd4a2d27cc4ead86d4c4a5
Nathan Winters <nathan.winters@tufts.edu>
2014-03-14 01:50:47 -0400

    checking my distance

--- transit/trains.js
+++ transit/trains.js
@@ -341,6 +341,7 @@ function calculateDistance(markers, myMarker)
               Math.sin(dLon/2) * Math.sin(dLon/2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c;    
+    console.log(d);
     if(d < closeDistance){
       console.log('found a new buddy');
       closeDistance = d;
