a59b5d247100d40fc0b07d1d32bab8170d824a1d
Nathan Winters <nathan.winters@tufts.edu>
2014-03-14 00:32:36 -0400

    trying the new table format

--- transit/trains.js
+++ transit/trains.js
@@ -222,8 +222,9 @@ function createTable(stationName, parsedResponse) {
       //  var tableTab = document.createElement('tbody');
         var content = new Array(parsedResponse["schedule"].length);
         for (var i = 0; i < parsedResponse["schedule"].length; i++) {
-          content[i] = '<p>parsedResponse['line']    parsedResponse['Schedule'][i]['TripID']   parsedResponse['Schedule'][i]['Destination']   ' +
-                          'parsedResponse['Schedule'][i]['Predictions'][k]['Seconds']</p>' 
+          content[i] = '<p>' + parsedResponse['line'] + '   ' + parsedResponse['Schedule'][i]['TripID'] + '   ' + 
+                          parsedResponse['Schedule'][i]['Destination'] + '   ' + 
+                          parsedResponse['Schedule'][i]['Predictions'][k]['Seconds'] + '</p>';
         }        
         var toReturn;
         for(var j = 0; j < content.length; j++){
